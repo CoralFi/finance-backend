7e20816c86ab5d0dfcaa091178aaf437fea36a1a
Macarena <macarenapazos33@gmail.com>
2024-12-12 23:01:30 -0300

    private key

--- services/utila/TokenService.js
+++ services/utila/TokenService.js
@@ -7,10 +7,6 @@ class TokenService {
         this.SERVICE_ACCOUNT_EMAIL = process.env.UTILA_SERVICE_ACCOUNT;
         this.UTILIA_API_URI = "https://api.utila.io/";
 
-
-        // Cargar clave privada
-        this.privateKey = Buffer.from(process.env.PRIVATE_KEY_BASE64, 'base64').toString('utf8');
-
         // Almacenar token en memoria para evitar regenerarlo en cada llamada
         this.token = null;
         this.expirationTime = null;
@@ -18,6 +14,13 @@ class TokenService {
 
     // Método para generar un nuevo token
     generateToken() {
+        // Cargar clave privada
+        if (!process.env.PRIVATE_KEY_BASE64) {
+            throw new Error('La variable de entorno PRIVATE_KEY_BASE64 no está definida');
+        }
+        
+        const privateKey = Buffer.from(process.env.PRIVATE_KEY_BASE64, 'base64').toString('utf8');
+
         const now = Math.floor(Date.now() / 1000);
 
         // Si el token es válido, no lo regeneramos
@@ -33,7 +36,7 @@ class TokenService {
         };
 
         // Firmar el token
-        this.token = jwt.sign(payload, this.privateKey, { algorithm: "RS256" });
+        this.token = jwt.sign(payload, privateKey, { algorithm: "RS256" });
         this.expirationTime = now + 3600; // Actualizar tiempo de expiración
         return this.token;
     }
