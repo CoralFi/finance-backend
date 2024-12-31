fea9b5425e4c77c9d893e8de104da3b304dac2e0
Macarena <macarenapazos33@gmail.com>
2024-12-31 13:58:38 -0300

    reset passwor

--- api/email/send-password-reset.js
+++ api/email/send-password-reset.js
@@ -8,6 +8,16 @@ const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 apiInstance.setApiKey(SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY);
 
 export default async function handler(req, res) {
+
+    res.setHeader('Access-Control-Allow-Origin', 'https://finance-front-beryl.vercel.app'); // TODO: cambiar por la del front
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+} 
+
   if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
 
   const { email } = req.body;
