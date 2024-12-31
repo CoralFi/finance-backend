873b57f2ed170aef5b3209cab713b9f65e4528a0
Macarena <macarenapazos33@gmail.com>
2024-12-31 13:10:08 -0300

    cors ajuste

--- api/auth/signup.js
+++ api/auth/signup.js
@@ -16,8 +16,8 @@ apiInstance.setApiKey(SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey, process.
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', 'https://finance-front-beryl.vercel.app'); // TODO: cambiar por la del front
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
     }
