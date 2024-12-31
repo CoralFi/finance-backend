f0cc67f9e8f794f77f0b2d46e217e3aa12e3127e
Gonzalo Arana <120038046+aranagonzalo@users.noreply.github.com>
2024-12-31 18:51:19 -0500

    Update send-password-reset.js

--- api/email/send-password-reset.js
+++ api/email/send-password-reset.js
@@ -9,7 +9,7 @@ apiInstance.setApiKey(SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey, process.
 
 export default async function handler(req, res) {
 
-    res.setHeader('Access-Control-Allow-Origin', 'https://finance-front-beryl.vercel.app'); // TODO: cambiar por la del front
+    res.setHeader('Access-Control-Allow-Origin', '*'); // TODO: cambiar por la del front
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     
