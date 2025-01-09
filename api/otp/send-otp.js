9bbb1501e83929476a60563cf65112c3e12ad580
Macarena <macarenapazos33@gmail.com>
2025-01-09 00:41:59 -0300

    update send-otp

--- api/otp/send-otp.js
+++ api/otp/send-otp.js
@@ -1,27 +1,28 @@
 import CreateOTPService from "../../services/otp/CreateOTPService.js";
 
 export default async function handler(req, res) {
-        if (req.method !== "POST") {
-            return res.status(405).json({message: "Método no permitido"});
-        }
+    
+    res.setHeader('Access-Control-Allow-Origin', '*'); //todo: cambiar por la del front
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-        res.setHeader('Access-Control-Allow-Origin', '*'); //todo: cambiar por la del front
-        res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    // Manejar solicitudes OPTIONS (preflight)
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
 
-        // Manejar solicitudes OPTIONS (preflight)
-        if (req.method === 'OPTIONS') {
-            return res.status(200).end();
-        }
+    const { email, clientId } = req.body;
+    const createOtpService = new CreateOTPService();
 
-        const { email, clientId } = req.body;
-        const createOtpService = new CreateOTPService();
-
+    if(req.method === "POST") {
         try {
             const otp = await createOtpService.createOtp(email, clientId);
             res.status(201).json({ message: "Código de validación enviado"});
         } catch (error) {
             res.status(500).json({ message: "Error al enviar el OTP"});
         }
+    } else {
+        return res.status(405).json({message: "Método no permitido"});
+    }
     
 }
