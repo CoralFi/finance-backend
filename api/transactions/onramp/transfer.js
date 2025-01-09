9178153e81ad1d57817d60f85dd6a373955eebed
Macarena <macarenapazos33@gmail.com>
2025-01-08 23:44:11 -0300

    update kyc

--- api/transactions/onramp/transfer.js
+++ api/transactions/onramp/transfer.js
@@ -1,4 +1,7 @@
 import CustomerService from "../../../services/sphere/CustomerService.js";
+import SupabaseClient from "../../../database/client.js";
+
+const supabase = new SupabaseClient().getClient();
 //consultar kyc y actualizar la base de datos
 //Devuelve los datos de la virtual account, en caso de que el usuario no tenga kyc o tos aprobado, devuelve un mensaje
 //En caso de que no tenga virtual account, se crea uno y se devuelve
@@ -19,11 +22,14 @@ export default async function handler(req, res) {
         const getStatus = await customerService.getKYCAndTOSStatus(customerId);
         const tos = getStatus.tosStatus;
         const kyc = getStatus.kycStatus;
+        console.log(kyc)
 
         const tosApprove = tos === "approved";
         const kycApprove = kyc === "approved";
         const kycIncomplete = kyc === "incomplete";
 
+        updateKyCStatus(kyc);
+        
         if(!tosApprove || !kycApprove) {
             return res.status(400).json({ message: "El usuario no aceptó los términos y condiciones o no completó el proceso de KYC" });
         }
@@ -41,4 +47,17 @@ export default async function handler(req, res) {
     } catch (error) {
         res.status(500).json({ message: `${error.message}` });
     }
+
+    async function updateKyCStatus(kyc) {
+        const { data, error } = await supabase
+            .from("usuarios")
+            .update({ estado_kyc: kyc })
+            .eq("customer_id", customerId);
+
+        if (error) {
+            throw new Error("Error al actualizar el estado del KYC");
+        }
+    }
 }
+
+
