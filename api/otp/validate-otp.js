92a2c557d0dd002a476a4f528ada060ea07a1658
Macarena <macarenapazos33@gmail.com>
2025-01-09 01:48:37 -0300

    update validate-otp

--- api/otp/validate-otp.js
+++ api/otp/validate-otp.js
@@ -1,10 +1,6 @@
 import ValidateOTPService from "../../services/otp/ValidateOTPService.js";
 
 export default async function handler(req, res) {
-        if (req.method !== "POST") {
-            return res.status(405).json({message: "Método no permitido"});
-        }
-
         res.setHeader('Access-Control-Allow-Origin', '*'); //todo: cambiar por la del front
         res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -14,14 +10,18 @@ export default async function handler(req, res) {
             return res.status(200).end();
         }
 
-        const { email, otp } = req.body;
-        const validateOtpService = new ValidateOTPService();
-
-        try {
-            const otpValidation = await validateOtpService.validateOtpService(email, otp);
-            res.status(201).json({ message: "OTP validado"});
-        } catch (error) {
-            res.status(500).json({ message: "Error al validar el OTP"});
+        if(req.method === "POST") {
+            const { email, otp } = req.body;
+            const validateOtpService = new ValidateOTPService();
+    
+            try {
+                const otpValidation = await validateOtpService.validateOtpService(email, otp);
+                res.status(201).json({ message: "OTP validado"});
+            } catch (error) {
+                res.status(500).json({ message: "Error al validar el OTP"});
+            }
+        } else {
+            return res.status(405).json({message: "Método no permitido"});
         }
     
 }
