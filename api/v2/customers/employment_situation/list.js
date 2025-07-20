eb0b6cf3d8decb525c41b4c9c0c7d79bc4d8f4ce
MagnusTerra <escalantejose22@gmail.com>
2025-07-20 17:07:34 -0600

    feat: add language-based sorting for employment situations list

--- api/v2/customers/employment_situation/list.js
+++ api/v2/customers/employment_situation/list.js
@@ -12,11 +12,23 @@ export default async function handler(req, res) {
     if (req.method !== 'GET') return res.status(405).send('Method Not Allowed');
 
     if (req.method === 'GET') {
+
+        const {lenguage} = req.query;
+
         try {
             const { data, error } = await supabase.rpc('get_active_employment_situations');
 
             if (error) {
                 return res.status(500).json({ error: error.message });
+            }
+
+            //order data alphabetically by lenguage
+            if (lenguage === 'es') {
+                data.sort((a, b) => a.es_label.localeCompare(b.es_label));
+            } else if (lenguage === 'en') {
+                data.sort((a, b) => a.en_label.localeCompare(b.en_label));
+            } else {
+                return res.status(200).json(data);
             }
 
             return res.status(200).json(data);
