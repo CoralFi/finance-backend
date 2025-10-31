e4bcf0a88cfc09b43b0ca9a2e1e0c7f3adafbb18
Pedro Ramos <pedrorc2018@gmail.com>
2025-10-31 17:08:16 -0500

    feat: enhance create transaction API documentation and improve error logging

--- src/api/bussiness/transactions/createTransactions.ts
+++ src/api/bussiness/transactions/createTransactions.ts
@@ -1,9 +1,45 @@
 import { Request, Response } from 'express';
 import conduitFinancial from '@/services/conduit/conduit-financial';
-
+/**
+ * Create a new transaction 
+ *{
+ *  "type": "withdrawal",
+ *  "destination":
+ *     {
+ *        "walletId":"wlt_34nnOseuR0AVKwRdgkHaUlax7YY",
+ *        "asset":"USDT",
+ *        "network":"ethereum",
+ *        "amount":"10.00"
+ *     }
+ *}
+ * ESTA PIDE UNA QUOTE COMO ESTA 
+ * {
+ * "source": {
+ *    "amount": "10.00",
+ *   "asset": "USD"
+ * },
+ * "target": {
+ *   "asset": "USDT",
+ *   "network": "ethereum"
+ * }
+ *}
+ * PUEDE SER WALLET O CUENTA BANCARIA, NO COMPRENDO MUY BIEN 
+ * {
+ * "type": "onramp",
+ * "quote": "quote_34qYtbnwvNia7Y3gaj8QmtW9Mi5",
+ * "source": "bank_34nPOhgCsRj7YSWiTowI8cTRKOZ",
+ * "destination": "bank_34nnOv4g8oH2R24DJrilY2qo82L",
+ * "purpose": "InterCompanyTransfer",
+ * "documents": [
+ *   "doc_34nkRui71EevJnkZVjmHQtAgaYb"
+ * ]
+ *}
+ * 
+ */
 export const createTransactionController = async (req: Request, res: Response): Promise<Response> => {
   try {
     const { type } = req.body;
+
 
     if (!type) {
       return res.status(400).json({
@@ -51,7 +87,7 @@ export const createTransactionController = async (req: Request, res: Response): 
         });
     }
 
-    const { data } = await conduitFinancial.createTransacions(req.body);
+    const data = await conduitFinancial.createTransacions(req.body);
 
     return res.status(201).json({
       success: true,
@@ -59,7 +95,10 @@ export const createTransactionController = async (req: Request, res: Response): 
       data,
     });
   } catch (error: any) {
-    console.error('Error creating transaction:', error.response.data);
+
+    console.error('data Enviada: ', error.response.config.data);
+    console.error('headers Enviados: ', error.response.request._header);
+    console.error('message Recibido', error.response.data)
     return res.status(500).json({
       success: false,
       message: 'Failed to create transaction',
